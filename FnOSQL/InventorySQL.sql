917647edb385358d26393c0b221e94826c2a786d
DHA1985 <90617533+DHA1985@users.noreply.github.com>
2025-10-14 13:51:13 +0700

    Create InventorySQL.sql

---
+++ FnOSQL/InventorySQL.sql
@@ -0,0 +1,22 @@
+
+
+select name from sys.tables where name like '%REFERENCECATEGORY%'
+
+
+select * from purchtable where purchid = ''
+select top 10 * from INVENTTRANSORIGIN where dataareaid = 'Usmf' and REFERENCECATEGORY = 2 and itemid = 'D0001' order by inventtransid desc
+select * from INVENTTRANSORIGINPURCHLINE where PURCHLINEINVENTTRANSID = 'BRMF-000011'
+select * from PURCHLINE where INVENTTRANSID = 'BRMF-000011'
+
+L where L.INVENTTRANSORIGIN = 22565484225
+select top 10 * from inventtrans where INVENTTRANSORIGIN = 22565424533
+
+InventTransOriginPurchLine or InventTransOrigin
+select top 10 
+
+
+select distinct REFERENCECATEGORY from INVENTTRANSORIGIN
+
+
+
+select * from INVENTTRANSORIGIN
